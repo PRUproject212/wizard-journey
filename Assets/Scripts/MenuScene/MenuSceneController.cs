f6ccb6b31d8907bfb4743879a7a5a20ab8285429
noproblem2311 <quanpva.dev@gmail.com>
2024-09-24 08:36:47 +0700

    update menu

--- Assets/Scripts/MenuScene/MenuSceneController.cs
+++ Assets/Scripts/MenuScene/MenuSceneController.cs
@@ -1,8 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 using UnityEngine.SceneManagement;
 
 public class MenuSceneController : MonoBehaviour
@@ -18,16 +16,48 @@ public class MenuSceneController : MonoBehaviour
     [SerializeField] private GameObject shopPopup;
     [SerializeField] private GameObject levelPopup;
 
-    void Start() {
-        PlayButton.onClick.AddListener(OnClickPlayButton);
-        CreditButton.onClick.AddListener(OnClickCreditButton);    
+    [Header("Hold Logic Settings")]
+    public float holdDuration = 1.0f; // Time required to hold the button (in seconds)
+
+    private bool isHoldingPlayButton = false;
+    private bool isHoldingCreditButton = false;
+    private float holdTimer = 0;
+
+    void Start()
+    {
+        // Attach functions to button events
+        PlayButton.onClick.AddListener(() => StartCoroutine(HoldButton(OnClickPlayButton, PlayButton)));
+        CreditButton.onClick.AddListener(() => StartCoroutine(HoldButton(OnClickCreditButton, CreditButton)));
         ExitButton.onClick.AddListener(OnClickExitButton);
-        LevelSelectButton.onClick.AddListener(OnClickLevelSelectButton);
+        LevelSelectButton.onClick.AddListener(() => StartCoroutine(HoldButton(OnClickLevelSelectButton, LevelSelectButton)));
+    }
+
+    private void Update()
+    {
+        // You can update hold logic here if necessary for other continuous actions
+    }
+
+    private IEnumerator HoldButton(System.Action action, Button button)
+    {
+        float holdTime = 0f;
+        while (holdTime < holdDuration)
+        {
+            if (!Input.GetMouseButton(0)) // Check if the button is released before the required hold time
+            {
+                yield break; // Exit the coroutine if button is released early
+            }
+
+            holdTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // Action happens only if the button is held for the required duration
+        action.Invoke();
     }
 
     private void OnClickPlayButton()
     {
-        SceneManager.LoadScene(Scenes.Level1.ToString());
+        SceneManager.LoadScene("Level1"); // Assuming SceneManager loads a scene by name
     }
 
     private void OnClickCreditButton()
@@ -37,21 +67,21 @@ public class MenuSceneController : MonoBehaviour
 
     private void OnClickExitButton()
     {
-        #if UNITY_STANDALONE
-                Application.Quit();
-        #endif
-        #if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;
-        #endif
+#if UNITY_STANDALONE
+            Application.Quit();
+#endif
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 
     private void OnClickShopButton()
     {
-        shopPopup.gameObject.SetActive(false);
+        shopPopup.SetActive(true); // Open shop pop-up on hold
     }
 
     private void OnClickLevelSelectButton()
     {
-        levelPopup.gameObject.SetActive(false);
+        levelPopup.SetActive(true); // Open level select pop-up on hold
     }
 }
