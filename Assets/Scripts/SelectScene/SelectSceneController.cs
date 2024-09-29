f3eda41d4fa92e4188370f3c4e982d3a47c2cdfe
Beka883 <hnbkhanh080803@gmail.com>
2024-09-29 10:09:19 +0700

    presentation2

--- Assets/Scripts/SelectScene/SelectSceneController.cs
+++ Assets/Scripts/SelectScene/SelectSceneController.cs
@@ -9,10 +9,12 @@ public class SelectSceneController : MonoBehaviour
     [Header("Button")]
     public Button ReturnButton;
     public Button Level1Button;
+    public Button Level2Button;
 
     private void Start() {
         ReturnButton.onClick.AddListener(OnClickReturnButton);
         Level1Button.onClick.AddListener(OnClickLevel1Button);
+        Level2Button.onClick.AddListener(OnClickLevel2Button);
     }
 
     private void OnClickReturnButton(){
@@ -23,5 +25,7 @@ public class SelectSceneController : MonoBehaviour
         SceneManager.LoadScene(Scenes.Level1.ToString());
     }
 
-
+    private void OnClickLevel2Button(){
+        SceneManager.LoadScene(Scenes.Level2.ToString());
+    }
 }
